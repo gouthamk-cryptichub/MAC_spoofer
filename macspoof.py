89bcb2b3152bdbe5a4e927e7600012014ba96ca8
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-20 12:46:38 -0400

    Added Command line args

--- macspoof.py
+++ macspoof.py
@@ -1,9 +1,12 @@
 #!/user/bin/env python
 import subprocess
+import optparse
 
-iface = input("Netwok Interface >")
-addr = input("New MAC >")
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="iface", help="Name of the interface to change its MAC")
+parser.add_option("-m", "--mac", dest="addr", help="The new MAC Address")
+(value, args) = parser.parse_args()
 
-subprocess.call("ifconfig " + iface + " down", shell=True)
-subprocess.call("ifconfig " + iface + " hw ether " + addr, shell=True)
-subprocess.call("ifconfig " + iface + " up", shell=True)
+subprocess.call("ifconfig " + value.iface + " down", shell=True)
+subprocess.call("ifconfig " + value.iface + " hw ether " + value.addr, shell=True)
+subprocess.call("ifconfig " + value.iface + " up", shell=True)
